39994eaa51cd567c9c6fe63c2559061f598d7c55
scag <virgovica@gmail.com>
2017-05-30 19:21:39 +0200

    Added description for set-envar.sh (inside file)

--- set-envar.sh
+++ set-envar.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# In case you have LinuX, I found this way to be very efficient.
+# Run the script to export environment variables for a current shell session
+# and all child processes. However, you cannot run it the usual way`
+# ./scriptname.sh
+# you want the script to be executed in current shell, above would execute
+# it in subshell
+# correct way:
+# . ./scriptname.sh
+# or 
+# source ./scriptname.sh
+
 export SECRET_KEY='super_secret_key'
 export MAIL_USERNAME='somehuman@mail.com'
 export MAIL_PASSWORD='strong_password'
