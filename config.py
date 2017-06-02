e3d7ff5015d0d654cc26c1b6ce6e67b0ad39876c
scag <virgovica@gmail.com>
2017-06-03 00:14:07 +0200

    Deleted old configurations

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
-# app.config.from_pyfile
 import os
-
-#SQLALCHEMY_DATABASE_URI="sqlite:///database.db"
-#SQLALCHEMY_TRACK_MODIFICATIONS=False
-#SECRET_KEY=os.environ.get("SECRET_KEY")
-#USE_SESSION_FOR_NEXT=True
-#MAIL_SERVER='smtp.gmail.com'
-#MAIL_PORT=465
-#MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
-#MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
-#MAIL_USE_TLS=False
-#MAIL_USE_SSL=True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
